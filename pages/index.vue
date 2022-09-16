6afa62dfc3beda1b26c47ee6ec61b498feeb6637
nlongn22 <nlongn22@icloud.com>
2022-09-16 21:53:36 +0200

    Update description

--- pages/index.vue
+++ pages/index.vue
@@ -21,20 +21,19 @@
         </section>
 
         <section>
-            <p class="description">
-                Hey there &#128075;
+            <p>
+                Hey there &#128075;.
                 <br />
                 My name is Ngoc Long Nguyen but most people call me Filip. I'm a
-                frontend developer who currently lives in Munich, Germany. I am
-                20 years old and after finishing high school in the summer of
-                2021, I decided to start learning the basics of web development,
-                mainly frontend.
+                20 year old frontend developer who currently lives in
+                <s>Munich</s> Berlin, Germany. At the moment I'm studying
+                computer science at the Technical University of Berlin.
                 <br />
                 <br />
                 In my free time (when I'm not learning anything) I like to watch
                 TV shows or hang out with my friends. I'm quite a big fan of
                 Formula 1 so every weekend is spent watching races and following
-                news. To stay in shape I try to run at least every other day.
+                news. To stay in shape I try to run every other day.
             </p>
         </section>
     </main>
